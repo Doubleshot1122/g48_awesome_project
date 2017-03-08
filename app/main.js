82f2af9941234595f246ce2032ab9ea2f1d1f651
Mike <mahathaway93@gmail.com>
2017-03-08 11:33:22 -0800

    further upgraded carousel code

--- app/main.js
+++ app/main.js
@@ -10,23 +10,30 @@ console.log("main.js is linked")
 // * If you move forward on the last image, it should go the first; if you move backwards on the first image, you should go to the last
 // * If you move forward on an image, the timer should reset (i.e. if the timer is set to rotate every five seconds and a user clicks on the fourth second, the image should change and wait another five seconds)
 
+//defined a scope limited object with methods for manipulating the carousel picture
 const carousel = (function(document){
   const carousel = {}
+  carousel.imagesArray = []
 
   //this function is incomplete
   function populateImageArray(){
-    const pictureOne = document.getElementById('carousel1')
-    const pictureTwo = document.getElementById('carousel2')
-    const pictureThree = document.getElementById('carousel3')
+    const pictureOne = "./assets/carousel1.jpg"
+    const pictureTwo = "./assets/carousel2.jpg"
+    const pictureThree = "./assets/carousel3.jpg"
 
-    return imagesArray.concat(pictureOne,pictureTwo,pictureThree)
+    //
+    // const pictureOne = document.getElementById('carousel1')
+    // const pictureTwo = document.getElementById('carousel2')
+    // const pictureThree = document.getElementById('carousel3')
+
+    return carousel.imagesArray.concat(pictureOne,pictureTwo,pictureThree)
   }
 
   populateImageArray()
-  carousel.imagesArray = []
   carousel.currentImage = carousel.imagesArray[0]
 
   carousel.switchImageRight = function(){
+    console.log(carousel.imagesArray)
     if(carousel.currentImage === carousel.imagesArray[0]){
       return displayImage(carousel.imagesArray[1])
     }
@@ -51,9 +58,11 @@ const carousel = (function(document){
   }
 
   function displayImage(picture){
-    //role-preview is a placeholder for
+    const imageContainer = document.getElementById("image-container")
     carousel.currentImage = picture
-    return document.getElementsByClassName("role-preview")[0].src=`${picture}`
+    console.log('we\'re displaying images!')
+    console.log(picture)
+    return imageContainer.src=`${picture}`
   }
 
   return carousel
@@ -64,11 +73,11 @@ window.setInterval(carousel.switchImageRight(),5000)
 
 //moves image to right or left
 $(document).ready(function(){
-  $('#buttonRight').on('click',function(){
+  $('#leftButton').on('click',function(){
     carousel.switchImageRight()
   })
 
-  $('#buttonLeft').on('click',function(){
+  $('#rightButton').on('click',function(){
     carousel.switchImageLeft()
   })
 })
